b4d1122e638a89dd1619bdf512c13e98a92b7a3f
Riven <riven@kittenbot.cc>
2023-12-11 19:20:47 +0800

    ext works with hardware v2

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 namespace tabbyrobot {
-    const TABBY_ADDR = 0x22
+    const TABBY_ADDR = 0x16
     const REG_MOTOR = 0x02
     const REG_SERVO1 = 0x03
     const REG_SERVO2 = 0x04
@@ -20,9 +20,9 @@ namespace tabbyrobot {
 
     export enum LeftRight {
         //% block='Left'
-        LEFT=0,
+        LEFT = 0,
         //% block='Right'
-        RGIHT=1,
+        RGIHT = 1,
     }
 
 
@@ -46,7 +46,7 @@ namespace tabbyrobot {
     //% group="Tabby"
     //% left.min=0 left.max=100
     //% right.min=0 right.max=100
-    export function frontlight(left: number, right: number){
+    export function frontlight(left: number, right: number) {
         let buf = pins.createBuffer(3)
         buf[0] = REG_HEADLIGHT
         buf[1] = left
@@ -61,7 +61,7 @@ namespace tabbyrobot {
     //% block="Motor $idx Left $left Right $right"
     //% left.shadow="speedPicker"
     //% right.shadow="speedPicker"
-    export function motorRun(left: number, right: number){
+    export function motorRun(left: number, right: number) {
         let buf = pins.createBuffer(5)
         // REG, M1A, M1B, M2A, M2B
         buf[0] = REG_MOTOR
@@ -85,6 +85,22 @@ namespace tabbyrobot {
     }
 
     /**
+     * Motor Stop
+     */
+    //% block="Motor Stop"
+    export function motorStop() {
+        let buf = pins.createBuffer(5)
+        // REG, M1A, M1B, M2A, M2B
+        buf[0] = REG_MOTOR
+        buf[1] = 0
+        buf[2] = 0
+        buf[3] = 0
+        buf[4] = 0
+
+        pins.i2cWriteBuffer(TABBY_ADDR, buf)
+    }
+
+    /**
      * Servo Degree
      */
     //% block="Servo $idx set to $degree"
@@ -92,14 +108,23 @@ namespace tabbyrobot {
     export function servoSet(idx: LeftRight, degree: number) {
         let buf = pins.createBuffer(3)
         buf[0] = idx == LeftRight.LEFT ? REG_SERVO1 : REG_SERVO2
-        let pulsePosition = 10309
         let minPulse = 600
         let maxPulse = 2400
         let v_us = (degree * (maxPulse - minPulse) / 180 + minPulse)
-        let value = Math.round(v_us * 256 / pulsePosition)
-        buf[1] = value & 0xff
-        buf[2] = value >> 8
+        buf[1] = v_us & 0xff
+        buf[2] = v_us >> 8
         pins.i2cWriteBuffer(TABBY_ADDR, buf)
     }
+
+    /**
+     * Line state
+     */
+    //% block="Line $idx"
+    export function line(idx: LeftRight): number {
+        let value = pins.analogReadPin(idx == LeftRight.LEFT ? AnalogPin.P1 : AnalogPin.P2)
+        return value
+    }
+
+
 
 }
