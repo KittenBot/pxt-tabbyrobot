e9cc080cd977e75c5344920175a3de8d7c4da26a
CCFIVE <jmmxdwu@gmail.com>
2023-12-25 01:19:13 +0800

    合并RGB分栏，修改blockid

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 //% color="#e76f51" weight=15 icon="\uf1b9"
-//% groups='["Leds", "Motors", "Sensor","IR"]'
+//% groups='["Leds", "RGB", "Motors", "Sensor","IR"]'
 //% block="TabbyRobot"
 
 namespace tabbyRobot {
@@ -12,9 +12,14 @@ namespace tabbyRobot {
 
     let inited = false
 
-    let neoStrip: neopixel.Strip;
+    //let neoStrip: neopixel.Strip;
     let distanceBuf = 0;
 	let irVal = 0
+    let neopixel_buf = pins.createBuffer(16 * 3);
+    for (let i = 0; i < 2 * 3; i++) {
+        neopixel_buf[i] = 0
+    }
+    let _brightness = 100
 
 
     export enum LeftRight {
@@ -72,6 +77,32 @@ namespace tabbyRobot {
     Nine = 26
 	}
 
+    /**
+     * colors for ws2812 strip
+     */
+    export enum RGBColors {
+        //% block=red
+        Red = 0xFF0000,
+        //% block=orange
+        Orange = 0xFFA500,
+        //% block=yellow
+        Yellow = 0xFFFF00,
+        //% block=green
+        Green = 0x00FF00,
+        //% block=blue
+        Blue = 0x0000FF,
+        //% block=indigo
+        Indigo = 0x4b0082,
+        //% block=violet
+        Violet = 0x8a2be2,
+        //% block=purple
+        Purple = 0xFF00FF,
+        //% block=white
+        White = 0xFFFFFF,
+        //% block=black
+        Black = 0x000000
+    }
+
     
     export function init() {
         if (inited) return 
@@ -85,23 +116,24 @@ namespace tabbyRobot {
     /**
      * Init RGB pixels on tabby robot
      */
-    //% blockId="tabby_rgb" block="ambient RGB"
+    //% blockId=tabby_rgb block="ambient RGB"
     //% group="Leds"  weight=63
     //% weight=200
+    /**
     export function rgb(): neopixel.Strip {
         if (!neoStrip) {
             neoStrip = neopixel.create(DigitalPin.P16, 2, NeoPixelMode.RGB)
         }
 
         return neoStrip;
-    }
+    }*/
 
     /**
      * Headlights control
      * @param left set brightness; eg: 100
      * @param right set brightness; eg: 100
      */
-    //% blockId="tabby_headlights" block="headlights left brightness $left right brightness $right"
+    //% blockId=tabby_headlights block="headlights left brightness $left right brightness $right"
     //% group="Leds"
     //% left.min=0 left.max=100
     //% right.min=0 right.max=100
@@ -119,7 +151,7 @@ namespace tabbyRobot {
     /**
     * Headlights all
     */
-    //% blockId="tabby_headlights_all" block="headlights all $enabled"
+    //% blockId=tabby_headlights_all block="headlights all $enabled"
     //% enabled.shadow=toggleOnOff
     //% group="Leds"
     //% weight=249
@@ -144,7 +176,7 @@ namespace tabbyRobot {
     * @param enabled set; eg: true
     * @param enabled2 set; eg: true
     */
-    //% blockId="tabby_headlights_onoff_control" block="headlights left $enabled right $enabled2"
+    //% blockId=tabby_headlights_onoff_control block="headlights left $enabled right $enabled2"
     //% group="Leds"
     //% enabled.shadow=toggleOnOff
     //% enabled2.shadow=toggleOnOff
@@ -171,8 +203,11 @@ namespace tabbyRobot {
 
     /**
      * Motor Speed
+     * @param left speed; eg: 30
+     * @param right speed; eg: 30
      */
-    //% blockId="tabby_motor_speed" block="motor $idx left speed $left right speed $right"
+    //% blockId=tabby_motor_speed
+    //% block="motor left speed $left \\% right speed $right \\%"
     //% group="Motors"
     //% left.shadow="speedPicker"
     //% right.shadow="speedPicker"
@@ -205,7 +240,7 @@ namespace tabbyRobot {
     /**
      * Motor Stop
      */
-    //% blockId="tabby_motor_stop" block="motor stop all"
+    //% blockId=tabby_motor_stop block="motor stop all"
     //% group="Motors"
     //% weight=330
     export function motorStop() {
@@ -225,7 +260,10 @@ namespace tabbyRobot {
      * Servo Degree
      * @param degree set; eg: 90
      */
-    //% blockId="tabby_servo_degree" block="servo $idx degree $degree"
+    //% blockId=tabby_servo_degree block="servo $idx degree $degree=protractorPicker °"
+    //% idx.fieldEditor="gridpicker"
+    //% idx.fieldOptions.width=100
+    //% idx.fieldOptions.columns=2
     //% group="Motors"
     //% degree.min=0 degree.max=180
     //% weight=300
@@ -244,7 +282,7 @@ namespace tabbyRobot {
     /**
      * Line state
      */
-    //% blockId="tabby_tracking_sensor" block="tracking sensor $idx"
+    //% blockId=tabby_tracking_sensor block="tracking sensor $idx"
     //% group="Sensor"
     //% weight=300
     //% idx.fieldEditor="gridpicker"
@@ -257,7 +295,7 @@ namespace tabbyRobot {
     /**
      * Battery voltage
      */
-    //% blockId="tabby_battery_voltage" block="battery voltage(V)"
+    //% blockId=tabby_battery_voltage block="battery voltage(V)"
     //% group="Sensor"
     //% weight=200
     export function battery(): number {
@@ -320,6 +358,7 @@ namespace tabbyRobot {
 
     //% group="IR"
     //% weight=100
+    //% blockId=tabby_ir_callback
     //% block="on IR receiving"
     export function irCallback(handler: () => void) {
         pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
@@ -339,12 +378,84 @@ namespace tabbyRobot {
      * get IR value
      */
     //% group="IR"
+    //% blockId=tabby_ir_button
     //% block="IR button $Button is pressed"
     //% weight=90
     //% Button.fieldEditor="gridpicker"
     //% Button.fieldOptions.columns=3
     export function irButton(Button: IrButtons): boolean {
         return (irVal & 0x00ff) == Button
+    }
+
+    /**
+     * Set all RGB
+     */
+    //% group="RGB"
+    //% blockId=tabby_rgb_show_color
+    //% block=" RGB show color |%rgb"
+    //% weight=200
+    export function rgbShowColor(rgb: RGBColors) {
+        let r = (rgb >> 16) * (_brightness / 255);
+        let g = ((rgb >> 8) & 0xFF) * (_brightness / 255);
+        let b = ((rgb) & 0xFF) * (_brightness / 255);
+        for (let i = 0; i < 2 * 3; i++) {
+            if ((i % 3) == 0)
+                neopixel_buf[i] = Math.round(g)
+            if ((i % 3) == 1)
+                neopixel_buf[i] = Math.round(r)
+            if ((i % 3) == 2)
+                neopixel_buf[i] = Math.round(b)
+        }
+        ws2812b.sendBuffer(neopixel_buf, DigitalPin.P16)
+    }
+
+    /**
+     * RGB brightness
+     * @param brightness to set , eg: 50
+     */
+    //% group="RGB"
+    //% brightness.min=0 brightness.max=255
+    //% blockId=tabby_set_rgb_brightness
+    //% block="set RGB brightness |%brightness"
+    //% weight=250
+    export function setRgbBrightness(brightness: number) {
+        _brightness = brightness;
+    }
+
+    /**
+     * clear all rgb
+     */
+    //% group="RGB"
+    //% blockId=tabby_clear_all_rgb
+    //% block="clear all RGB"
+    //% weight=260
+    export function clearAllRgb() {
+        rgbShowColor(0)
+    }
+
+    /**
+     * Adjust the color of the RGB light separately
+     * @param index  , eg: 1
+     */
+    //% group="RGB"
+    //% index.min=1 index.max=2
+    //% blockId=tabby_set_index_color
+    //% block="RGB NO.|%index show color|%rgb"
+    //% weight=190
+    export function setIndexColor(index: number, rgb: RGBColors) {
+        let f = index-1;
+        let t = index-1;
+        let r = (rgb >> 16) * (_brightness / 255);
+        let g = ((rgb >> 8) & 0xFF) * (_brightness / 255);
+        let b = ((rgb) & 0xFF) * (_brightness / 255);
+
+        for (let i = f; i <= t; i++) {
+            neopixel_buf[i * 3 + 0] = Math.round(g)
+            neopixel_buf[i * 3 + 1] = Math.round(r)
+            neopixel_buf[i * 3 + 2] = Math.round(b)
+        }
+        ws2812b.sendBuffer(neopixel_buf, DigitalPin.P16)
+
     }
 
 
